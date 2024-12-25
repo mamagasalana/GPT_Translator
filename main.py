b0cef8aa3681ce1d9ff208d2ad4bde6402800c81
mamagasalana <ytee@butterfly.com.hk>
2024-12-25 22:04:29 +0800

    gpt to translate jp novel

---
+++ main.py
@@ -0,0 +1,116 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+from novel import NOVEL
+import time
+# Specify the path to your user profile directory
+USER_DATA_DIR = r"C:\Users\ASUS\Desktop\Research\GPT_Translator\user_data"
+
+class GPT_HANDLER:
+    def __init__(self):
+        options = uc.ChromeOptions()
+        options.add_argument(f"--user-data-dir={USER_DATA_DIR}")  # Path to the user data directory
+        options.add_argument("--profile-directory=Default")      # Use the default profile
+                
+        # Initialize undetected ChromeDriver
+        self.driver = uc.Chrome(options=options)
+        self.driver.maximize_window()
+        self.ac = ActionChains(self.driver)
+
+    def wait_response_done(self):
+        x3 = '//button[@data-testid="composer-speech-button"]|//button[@data-testid="send-button" and @disabled]'
+        while True:
+            try:
+                elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x3)))
+                break
+            except:
+                time.sleep(10)
+
+    def verify_text_sent(self, part):
+        if part:
+            x = f'//p[contains(text(), "{part}")]'
+            try:
+                EC.presence_of_element_located((By.XPATH, x))(self.driver)
+                return True
+            except:
+                return False
+        else:
+            return True
+        
+    def new_chat(self, txt):
+        self.driver.get("https://chatgpt.com/g/g-p-6767cd8419d0819189eaa12acdc44a23-i-parry-everything/project")
+        x1 = '//p[@data-placeholder="New chat in this project"]'
+        # x1 = '//textarea[@placeholder="New chat in this project"]'
+        x2 = '//button[@data-testid="send-button"]'
+        
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x1)))
+        for part in txt.split('\n'):
+            while True:
+                elm.send_keys(part)
+                if self.verify_text_sent(part):
+                    break
+            elm.send_keys(Keys.SHIFT + Keys.ENTER)
+            self.ac.reset_actions()
+                # if self.verify_text_sent(part):
+                #     break
+
+        
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x2)))
+        elm.click()
+        
+
+    def resume_chat(self, txt):
+        # x1 = '//textarea[@placeholder="Message ChatGPT"]'
+        x1 = '//p[@data-placeholder="Message ChatGPT"]'
+        x2 = '//button[@data-testid="send-button"]'
+        
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x1)))
+        for part in txt.split('\n'):
+            elm.send_keys(part)
+            elm.send_keys(Keys.SHIFT + Keys.ENTER)
+            self.ac.reset_actions()
+            # if self.verify_text_sent(part):
+            #     break
+
+            
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x2)))
+        elm.click()
+        
+
+    def main(self, page):
+        n = NOVEL(page=page)
+        pages= []
+        for data in n.iter_page():
+            page = data['page']
+            txt = data['data']
+
+            if page not in pages:
+                if len(pages) == 4:
+                    pages = []
+                    return #may have hit gpt limit
+                
+                if not pages:
+                    first = True
+
+                pages.append(page)
+            
+            if first:
+                self.new_chat(txt)
+                first=False
+            else:
+                self.resume_chat(txt)
+            self.wait_response_done()
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <page_start>")
+        sys.exit(1)
+    gpt = GPT_HANDLER()
+    page = sys.argv[1]  
+    gpt.main(int(page))
+    print("Done")
---
+++ novel.py
@@ -0,0 +1,87 @@
+import requests
+from lxml import html
+import logging
+
+logging.basicConfig(
+     filename='page.log',
+     level=logging.INFO, 
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+     datefmt='%H:%M:%S'
+ )
+
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Sec-CH-UA": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    "Sec-CH-UA-Mobile": "?0",
+    "Sec-CH-UA-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+}
+class NOVEL:
+    def __init__(self, url='https://ncode.syosetu.com/n6537fu/', page=1):
+        self.url = url
+        self.page= page
+        self.session = requests.session()
+
+    def iter_page(self):
+        while True:
+            url2 = self.url + str(self.page)
+            logging.info(url2)
+            r=  self.session.get(url2, headers=HEADERS)
+            for content in self.iter_content(r.content):
+                if 'error' in content:
+                    return
+                else:
+                    content['page'] = self.page
+                    yield content
+            self.page+=1
+
+    def iter_content(self, cnt, max_length=1800):
+        ret = {'data' : None}
+        tree = html.fromstring(cnt)
+        if tree.xpath('//h1[text()="エラー"]'):
+            ret['error'] = True
+            return ret
+
+        x2 = '//div[@class="js-novel-text p-novel__text"]'
+        lines = tree.xpath(x2)[0].text_content().split('\n')
+        grouped_text = []
+        current_group = []
+        current_length = 0
+
+        for line in lines:
+            line_length = len(line)
+            # Check if adding this line would exceed the max length
+            if current_length + line_length + len(current_group) > max_length:
+                # Save the current group and start a new one
+                grouped_text.append("\n".join(current_group))
+                current_group = [line]
+                current_length = line_length
+            else:
+                # Add line to the current group
+                current_group.append(line)
+                current_length += line_length
+
+        # Add the last group if not empty
+        if current_group:
+            grouped_text.append("\n".join(current_group))
+
+        for g in grouped_text:
+            ret['data'] = g
+            yield ret
+
+if __name__ =='__main__':
+    a = NOVEL()
+    for xx in a.iter_page():
+        print(xx)
+
+
