2512b33d8dc01185bf69ea3a393410ee71f7fb71
mamagasalana <ytee@butterfly.com.hk>
2024-12-26 23:14:09 +0800

    validate language

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
-
+from langdetect import detect, detect_langs
 from novel import NOVEL
 import time
 import os
@@ -28,12 +28,75 @@ class GPT_HANDLER:
 
     def wait_response_done(self):
         x3 = '//button[@data-testid="composer-speech-button"]|//button[@data-testid="send-button" and @disabled]'
+        xret ='//div[@data-message-author-role="assistant"]'
+        x2 = '//button[@aria-label="Edit message"]'
+        xwhite = '//div[@class="whitespace-pre-wrap"]'
+        xgrid = '//div[(contains(@class, "group/conversation")) and (.//div[text()="Send"])]//textarea'
+        xsend= '//button[./*[text()="Send"]]'
+        xbottom = '//button[contains(@class, "bg-token-main-surface-primary")]'
+        xbad = '//button[@aria-label="Bad response"]'
+
         while True:
+            while True:
+                try:
+                    elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x3)))
+                    break
+                except:
+                    time.sleep(10)
+
+            #verify if mandarin
             try:
-                elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x3)))
-                break
+                elm = WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.XPATH, xbottom)))
+                elm.click()
+                time.sleep(1)
             except:
-                time.sleep(10)
+                print("button not found?")
+            
+            elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, xret)))
+            elm2 = WebDriverWait(elm[-1], 10).until(EC.visibility_of_all_elements_located((By.XPATH, './/p')))
+            if not elm2:
+                print("no response found?")
+                continue
+
+            validation = []
+            for elm22 in elm2:
+                verify_txt = elm22.text
+                if verify_txt:
+                    lang = detect(verify_txt)
+                    validation.append(lang)
+
+            if len(validation) == 0:
+                print("no response found2?")
+                continue
+
+            score = sum([1 if 'zh' in lang else 0 for lang in validation ] ) / len(validation)
+            if score > 0.8:
+                return
+            else:
+                print('not mandarin: %s' % verify_txt)
+                elm = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xbad)))
+                self.driver.execute_script("arguments[0].scrollIntoView();", elm[-2])
+
+                elm = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xwhite)))
+                self.ac.move_to_element(elm[-1]).perform()
+
+                elm = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, x2)))
+                elm[-1].click()
+
+                elm = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xgrid)))
+                elm = elm[-1]
+                
+                part = '请翻译以下成中文：'
+                if not part in elm.text:
+                    elm.send_keys(Keys.CONTROL + Keys.HOME)
+                    self.ac.reset_actions()
+                    elm.send_keys(part)
+                    elm.send_keys(Keys.SHIFT + Keys.ENTER)
+                    self.ac.reset_actions()
+                    
+                elm = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xsend)))
+                elm[-1].click()
+
 
     def verify_text_sent(self, part):
         if part:
@@ -45,12 +108,17 @@ class GPT_HANDLER:
                 return False
         else:
             return True
-        
+    
+    def send_msg(self):
+        x2 = '//button[@data-testid="send-button"]'
+        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x2)))
+        elm.click()
+
     def new_chat(self, txt):
         self.driver.get("https://chatgpt.com/g/g-p-6767cd8419d0819189eaa12acdc44a23-i-parry-everything/project")
         x1 = '//p[@data-placeholder="New chat in this project"]'
         # x1 = '//textarea[@placeholder="New chat in this project"]'
-        x2 = '//button[@data-testid="send-button"]'
+        
         
         elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x1)))
         for part in txt.split('\n'):
@@ -63,15 +131,13 @@ class GPT_HANDLER:
                 # if self.verify_text_sent(part):
                 #     break
 
-        
-        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x2)))
-        elm.click()
+        self.send_msg()
+
         
 
     def resume_chat(self, txt):
         # x1 = '//textarea[@placeholder="Message ChatGPT"]'
         x1 = '//p[@data-placeholder="Message ChatGPT"]'
-        x2 = '//button[@data-testid="send-button"]'
         
         elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x1)))
         for part in txt.split('\n'):
@@ -81,10 +147,7 @@ class GPT_HANDLER:
             # if self.verify_text_sent(part):
             #     break
 
-            
-        elm = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, x2)))
-        elm.click()
-        
+        self.send_msg()
 
     def main(self, page):
         n = NOVEL(page=page)
